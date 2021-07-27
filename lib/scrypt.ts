9dde68dd8ed16262a6612dbd6edc3fcf6c3e4a1b
opliko <opliko.reg@protonmail.com>
2021-07-27 23:53:26 +0200

    :pushpin: pin all dependencies to current version
    Ensures issues like #4 won't happen again

--- lib/scrypt.ts
+++ lib/scrypt.ts
@@ -1,14 +1,14 @@
 import init, { source, scrypt as scryptWASM } from "./_wasm/wasm.js";
-import { encode as base64encode } from "https://deno.land/std/encoding/base64.ts";
-import { encode as hexencode } from "https://deno.land/std/encoding/hex.ts";
+import { encode as base64encode } from "https://deno.land/std@0.103.0/encoding/base64.ts";
+import { encode as hexencode } from "https://deno.land/std@0.103.0/encoding/hex.ts";
 const encoder: TextEncoder = new TextEncoder();
 const decoder: TextDecoder = new TextDecoder("utf-8");
 await init(source);
-export type encoding = ("utf-8" | "base64" | "hex");
+export type encoding = "utf-8" | "base64" | "hex";
 /**
  * Scrypt implementation using web assembly
  * @param {string|Uint8Array} password - string to hash
- * @param {string|Uint8Array} salt - 
+ * @param {string|Uint8Array} salt -
  * @param {number} N - CPU/Memory cost parameter. Must be a power of 2 smaller than 2^(128*r/8)
  * @param {number} r - block size
  * @param {number} p - parallelism factor
@@ -17,26 +17,27 @@ export type encoding = ("utf-8" | "base64" | "hex");
  * @returns {Promise<string|Uint8Array>} - the resulting hash encoded according to outputEncoding
  */
 export async function scrypt(
-  password: (string | Uint8Array),
-  salt: (string | Uint8Array),
-  N: number,
-  r: number,
-  p: number,
-  dklen?: number,
-  outputEncoding?: encoding,
-): Promise<(Uint8Array | string)> {
-  dklen = dklen ?? 64;
-  password = typeof password === "string" ? encoder.encode(password) : password;
-  salt = typeof salt === "string" ? encoder.encode(salt) : salt;
-  const result: Uint8Array = scryptWASM(password, salt, N, r, p, dklen);
-  switch (outputEncoding) {
-    case "base64":
-      return base64encode(result);
-    case "hex":
-      return hexencode(result);
-    case "utf-8":
-      return decoder.decode(result);
-    default:
-      return result;
-  }
+    password: string | Uint8Array,
+    salt: string | Uint8Array,
+    N: number,
+    r: number,
+    p: number,
+    dklen?: number,
+    outputEncoding?: encoding
+): Promise<Uint8Array | string> {
+    dklen = dklen ?? 64;
+    password =
+        typeof password === "string" ? encoder.encode(password) : password;
+    salt = typeof salt === "string" ? encoder.encode(salt) : salt;
+    const result: Uint8Array = scryptWASM(password, salt, N, r, p, dklen);
+    switch (outputEncoding) {
+        case "base64":
+            return base64encode(result);
+        case "hex":
+            return hexencode(result);
+        case "utf-8":
+            return decoder.decode(result);
+        default:
+            return result;
+    }
 }
--- mod_test.ts
+++ mod_test.ts
@@ -1,60 +1,60 @@
 import {
-  assertEquals,
-  assert,
-} from "https://deno.land/std/testing/asserts.ts";
+    assertEquals,
+    assert,
+} from "https://deno.land/std@0.103.0/testing/asserts.ts ";
 import { hash, verify } from "./mod.ts";
 
 Deno.test("basic hashing - scrypt format", async (): Promise<void> => {
-  const hashedPassword = await hash("test-password");
-  assert(await verify("test-password", hashedPassword));
+    const hashedPassword = await hash("test-password");
+    assert(await verify("test-password", hashedPassword));
 });
 Deno.test("basic hashing - PHC format", async (): Promise<void> => {
-  const hashedPassword = await hash("test-password", undefined, "phc");
-  assert(await verify("test-password", hashedPassword));
+    const hashedPassword = await hash("test-password", undefined, "phc");
+    assert(await verify("test-password", hashedPassword));
 });
 
 Deno.test("basic hashing raw", async (): Promise<void> => {
-  const hashedPassword = await hash("test-password", { salt: "test" }, "raw");
-  assertEquals(
-    hashedPassword,
-    "zu8gd0RTeX6r0dbNzBv5ZzXOAQo0UnFUw49uRXrPwAlDocpJSA43WEgAcKNlsBhLyA+zVDluz/0GFa1ShAcr6g==",
-  );
+    const hashedPassword = await hash("test-password", { salt: "test" }, "raw");
+    assertEquals(
+        hashedPassword,
+        "zu8gd0RTeX6r0dbNzBv5ZzXOAQo0UnFUw49uRXrPwAlDocpJSA43WEgAcKNlsBhLyA+zVDluz/0GFa1ShAcr6g=="
+    );
 });
 // source of this test string: https://github.com/barrysteyn/node-scrypt#what-are-the-essential-properties-for-storing-passwords
 Deno.test("verify scrypt string", async (): Promise<void> => {
-  assert(
-    await verify(
-      "password1",
-      "c2NyeXB0AAwAAAAIAAAAATpP+fdQAryDiRmCmcoOrZa2mZ049KdbA/ofTTrATQQ+m0L/gR811d0WQyip6p2skXVEMz2+8U+xGryFu2p0yzfCxYLUrAaIzaZELkN2M6k0",
-    ),
-  );
+    assert(
+        await verify(
+            "password1",
+            "c2NyeXB0AAwAAAAIAAAAATpP+fdQAryDiRmCmcoOrZa2mZ049KdbA/ofTTrATQQ+m0L/gR811d0WQyip6p2skXVEMz2+8U+xGryFu2p0yzfCxYLUrAaIzaZELkN2M6k0"
+        )
+    );
 });
 // source of this test string: https://passlib.readthedocs.io/en/stable/lib/passlib.hash.scrypt.html
 Deno.test("verify PHC string", async (): Promise<void> => {
-  assert(
-    await verify(
-      "password",
-      "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E",
-    ),
-  );
+    assert(
+        await verify(
+            "password",
+            "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E"
+        )
+    );
 });
 
 Deno.test("reject invalid password (PHC)", async (): Promise<void> => {
-  assertEquals(
-    await verify(
-      "invalid-password",
-      "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E",
-    ),
-    false,
-  );
+    assertEquals(
+        await verify(
+            "invalid-password",
+            "$scrypt$ln=16,r=8,p=1$aM15713r3Xsvxbi31lqr1Q$nFNh2CVHVjNldFVKDHDlm4CbdRSCdEBsjjJxD+iCs5E"
+        ),
+        false
+    );
 });
 
 Deno.test("reject invalid password (scrypt)", async (): Promise<void> => {
-  assertEquals(
-    await verify(
-      "invalid-password",
-      "c2NyeXB0AAwAAAAIAAAAATpP+fdQAryDiRmCmcoOrZa2mZ049KdbA/ofTTrATQQ+m0L/gR811d0WQyip6p2skXVEMz2+8U+xGryFu2p0yzfCxYLUrAaIzaZELkN2M6k0",
-    ),
-    false,
-  );
+    assertEquals(
+        await verify(
+            "invalid-password",
+            "c2NyeXB0AAwAAAAIAAAAATpP+fdQAryDiRmCmcoOrZa2mZ049KdbA/ofTTrATQQ+m0L/gR811d0WQyip6p2skXVEMz2+8U+xGryFu2p0yzfCxYLUrAaIzaZELkN2M6k0"
+        ),
+        false
+    );
 });
