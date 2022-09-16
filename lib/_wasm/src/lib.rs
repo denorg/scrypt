1a84a1556cff5b7e3fd0422c0273f6910a97060e
Opliko <opliko.reg@protonmail.com>
2022-09-16 19:25:23 +0000

    fix: remove wee_alloc as it's unmaintained
    wee_alloc is no longer maintained. There are currently no production-ready alternatives to std, the main one to watch for is lol_alloc.

--- lib/_wasm/src/lib.rs
+++ lib/_wasm/src/lib.rs
@@ -6,13 +6,6 @@ use scrypt::{scrypt, Params};
 use std::iter::repeat;
 use wasm_bindgen::prelude::*;
 
-cfg_if! {
-    if #[cfg(feature = "wee_alloc")] {
-        #[global_allocator]
-        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-    }
-}
-
 #[wasm_bindgen]
 pub fn scrypt_hash(password: &[u8], salt: &[u8], n: u32, r: u32, p: u32, dklen: usize) -> Vec<u8> {
     let log_n = (32 - n.leading_zeros() - 1) as u8;
