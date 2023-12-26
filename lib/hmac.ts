0118de750075ffa285cd487afad91a2b242dc582
Opliko <opliko.reg@protonmail.com>
2023-12-26 15:50:16 +0100

    docs: add more JSDoc

--- lib/hmac.ts
+++ lib/hmac.ts
@@ -1,14 +1,27 @@
 /**
- * A very primitive crypto.subtle.digestSync-based HMAC-SHA256 synchronous implementation.
+ * @author oplik0
+ * @description A very primitive crypto.subtle.digestSync-based HMAC-SHA256 synchronous implementation.
  */
 import { crypto } from "../deps.ts";
 
+/**
+ * Join two arrays at the end of the first one
+ * @param {Uint8Array} a - first array
+ * @param {Uint8Array} b - second array
+ * @returns {Uint8Array} - joined array of length a.length + b.length
+ */
 function mergeArrays(a: Uint8Array, b: Uint8Array): Uint8Array {
   const result = new Uint8Array(a.length + b.length);
   result.set(a);
   result.set(b, a.length);
   return result;
 }
+/**
+ * Ensure that key is of the correct length
+ * @param {Uint8Array} key
+ * @param {Uint8Array} blockSize length to get the key to
+ * @returns
+ */
 function blockSizedKey(key: Uint8Array, blockSize: number): Uint8Array {
   if (key.length > blockSize) {
     return new Uint8Array(crypto.subtle.digestSync("SHA-256", key));
@@ -20,6 +33,13 @@ function blockSizedKey(key: Uint8Array, blockSize: number): Uint8Array {
   return key;
 }
 
+/**
+ * Synchronous Hash-based Message Authentication Code implementation using SHA256
+ * @see https://en.wikipedia.org/wiki/HMAC
+ * @param {Uint8Array} key
+ * @param {Uint8Array} data
+ * @returns {Uint8Array} - HMAC-SHA256 of data using key
+ */
 export function hmacSHA256(key: Uint8Array, data: Uint8Array): Uint8Array {
   const b_key = blockSizedKey(key, 64);
   const o_pad = new Uint8Array(64);
