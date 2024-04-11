3e475c6993b3185001aceb5ff0b567a5d8dca208
Opliko <opliko.reg@protonmail.com>
2024-04-11 18:12:10 +0200

    fix: deno publish not working due to dirty git tree
    This reverts commit 3294defa71d39bd9e30ecfc5dadfca3e908e78e3 and does what it claimed instead...

--- example.ts
+++
@@ -1,9 +0,0 @@
-import { hash, verify } from "https://deno.land/x/scrypt@v4.3.4/mod.ts";
-
-console.time("hash");
-const hashResult = hash("password", { logN: 14 });
-console.timeEnd("hash");
-
-console.time("verify");
-const verifyResult = verify("password", hashResult);
-console.timeEnd("verify");
